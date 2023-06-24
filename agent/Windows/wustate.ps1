6a3a46ba0d7dc8086e2cba85143199080e22b409
yoyox78 <126859114+Yoyox78@users.noreply.github.com>
2023-06-24 08:02:31 +0200

    Add files via upload
    Ajout de scommentaires

--- agent/Windows/wustate.ps1
+++ agent/Windows/wustate.ps1
@@ -1,5 +1,7 @@
+# je vide la variable erreur
 $Error.Clear()
 
+# Verification du plugin pswindowsupdate, si il est présent ou pas
 $ModInstallSrvRoot = Get-Module -ListAvailable -Name PSWindowsUpdate
 if  (($Error.count -eq 0) -and ($ModInstallSrvRoot -eq $null))
 { 
@@ -11,9 +13,10 @@ if  (($Error.count -eq 0) -and ($ModInstallSrvRoot -eq $null))
 
 $Error.Clear()
 
+# 2e Verification pour verifier qu'il c'est bien installé
 $InvokeTest = Get-Module -ListAvailable -Name PSWindowsUpdate
 
-
+# Si il ne c'est pas installer je fais de modification et des installations suivant le message d'erreur
 if  (($Error.count -eq 0) -and ($InvokeTest -eq $null))
 { 
     $Error.Clear()
@@ -30,19 +33,27 @@ if  (($Error.count -eq 0) -and ($InvokeTest -eq $null))
 
 }
 
+# Récupérationdu module qui me permet de savoir si le poste est en attente de restart liée aux mise à jour
 if (-Not (Get-Module -ListAvailable -Name PendingReboot))
 { 
     Install-Module PendingReboot -Confirm:$false -Force
     Import-module PendingReboot
 }
 
+# Récupère la liste des MAJ
 $Winupdate = Get-WindowsUpdate
+# On prend les version de KB
 $WUTitre = $Winupdate.title | select-string -pattern 'KB[0-9]+'
+# On met en variable les valeur récupéré avec la regex
 $WUKB = $WUTitre.matches.value | Select-Object -Unique | Out-String 
+# On remplace les retour à la ligne par des espace
 $WUKB = $WUKB -replace '\s',' '
+# On  met en variable la valeur a savoir si le poste est en attente de reboot suite à des MAJ
 $WUReboot = (Test-PendingReboot -SkipConfigurationManagerClientCheck -Detailed).WindowsUpdateAutoUpdate
+# On récupère le serveur WSUS présent dans le registre
 $Server = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\" -Name "WUServer").WUServer
 
+# Si il y a des MAJ alors on met en variable NOK, sinon c'est OK
 if ($WUTitre.Count -gt 0)
 {
     $WUpdate = "NOK"
@@ -52,7 +63,7 @@ else
     $WUpdate = "OK"
 }
 
-
+#On renvoi le XML contenant les informations
 $xml = "<WUSTATE>`n"
 $xml += "<DATE>$(Get-Date -Format "dd/MM/yyyy_HH:mm")</DATE>`n"
 $xml += "<STATE>${WUpdate}</STATE>`n"
